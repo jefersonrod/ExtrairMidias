b64fc20bb76055fb54e6feffcd437ad7fcc2718d
Jeferson <jefersonrod@yahoo.com.br>
2017-11-04 20:04:21 -0200

    Extrair Midias - Prototipo Inicial
    Base para resolução do exercicio de extrair midias.

---
+++ ExtrairMidias/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace ExtrairMidias
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbCaminho = new System.Windows.Forms.TextBox();
+            this.btOpen = new System.Windows.Forms.Button();
+            this.btGo = new System.Windows.Forms.Button();
+            this.rtConteudo = new System.Windows.Forms.RichTextBox();
+            this.cbPDF = new System.Windows.Forms.CheckBox();
+            this.cbRAR = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // tbCaminho
+            // 
+            this.tbCaminho.Location = new System.Drawing.Point(94, 13);
+            this.tbCaminho.Name = "tbCaminho";
+            this.tbCaminho.Size = new System.Drawing.Size(569, 20);
+            this.tbCaminho.TabIndex = 0;
+            // 
+            // btOpen
+            // 
+            this.btOpen.Location = new System.Drawing.Point(13, 9);
+            this.btOpen.Name = "btOpen";
+            this.btOpen.Size = new System.Drawing.Size(75, 23);
+            this.btOpen.TabIndex = 1;
+            this.btOpen.Text = "Open";
+            this.btOpen.UseVisualStyleBackColor = true;
+            this.btOpen.Click += new System.EventHandler(this.btOpen_Click);
+            // 
+            // btGo
+            // 
+            this.btGo.Location = new System.Drawing.Point(669, 9);
+            this.btGo.Name = "btGo";
+            this.btGo.Size = new System.Drawing.Size(75, 23);
+            this.btGo.TabIndex = 2;
+            this.btGo.Text = "GO";
+            this.btGo.UseVisualStyleBackColor = true;
+            this.btGo.Click += new System.EventHandler(this.btGo_Click);
+            // 
+            // rtConteudo
+            // 
+            this.rtConteudo.Location = new System.Drawing.Point(13, 75);
+            this.rtConteudo.Name = "rtConteudo";
+            this.rtConteudo.Size = new System.Drawing.Size(731, 250);
+            this.rtConteudo.TabIndex = 3;
+            this.rtConteudo.Text = "";
+            // 
+            // cbPDF
+            // 
+            this.cbPDF.AutoSize = true;
+            this.cbPDF.Location = new System.Drawing.Point(13, 52);
+            this.cbPDF.Name = "cbPDF";
+            this.cbPDF.Size = new System.Drawing.Size(50, 17);
+            this.cbPDF.TabIndex = 4;
+            this.cbPDF.Text = ".PDF";
+            this.cbPDF.UseVisualStyleBackColor = true;
+            // 
+            // cbRAR
+            // 
+            this.cbRAR.AutoSize = true;
+            this.cbRAR.Location = new System.Drawing.Point(94, 52);
+            this.cbRAR.Name = "cbRAR";
+            this.cbRAR.Size = new System.Drawing.Size(52, 17);
+            this.cbRAR.TabIndex = 5;
+            this.cbRAR.Text = ".RAR";
+            this.cbRAR.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(753, 337);
+            this.Controls.Add(this.cbRAR);
+            this.Controls.Add(this.cbPDF);
+            this.Controls.Add(this.rtConteudo);
+            this.Controls.Add(this.btGo);
+            this.Controls.Add(this.btOpen);
+            this.Controls.Add(this.tbCaminho);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbCaminho;
+        private System.Windows.Forms.Button btOpen;
+        private System.Windows.Forms.Button btGo;
+        private System.Windows.Forms.RichTextBox rtConteudo;
+        private System.Windows.Forms.CheckBox cbPDF;
+        private System.Windows.Forms.CheckBox cbRAR;
+    }
+}
+
---
+++ ExtrairMidias/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExtrairMidias
+{
+    public partial class Form1 : Form
+    {
+        #region Variables
+        String htmlFile = @"c:\tmp\enem3.html";
+        String pdf = ".pdf";
+        String rar = ".rar";
+        String mp3 = ".mp3";
+        String HTTP = "http://";
+        String HREF = "href=";
+        
+        String pl = Environment.NewLine; //pula linha para lazy coders
+        #endregion
+
+                               
+        public Form1()
+        {
+            InitializeComponent();
+            tbCaminho.Text = htmlFile;
+        }
+
+        #region Function SearchReference()
+        public void SearchReference(String Extension)
+        {
+            String content;
+            String line;
+            String lineLink;
+            int countLine = 0;
+            int startLine = 0;
+            int finishLine = 0;
+            int sizeLine = 0;
+            int sizeString = 0;
+            int startSubstring = 0;
+            String tempContent = "";
+            int countOccurrence = 0;
+
+            StreamReader str = new StreamReader(htmlFile);//Le o arquivo e joga dentro de str
+            while ((content = str.ReadLine()) != null)//Le o str linha a linha e joga em content até encontrar null que é o final do arquivo
+            {
+                countLine++; //contando as linhas
+                line = content; //recebendo o conteudo do html linha a linha
+                while (line.Contains(HTTP) && line.Contains(Extension)) //enquanto ainda tiver http e pdf na string continua a buscar links
+                {
+                    if (line.Contains(HTTP) && line.Contains(Extension)) //enquanto ainda tiver http e pdf na string continua a buscar links
+                    {
+                        sizeLine = line.Length; //pega o tamanho da variavel line
+                        String cabecalho = (HREF + "\"" + HTTP); // cria variavel para achar a sequencia href + http
+                        startLine = line.IndexOf(cabecalho) + HREF.Length + 1; //marca start da linha + tamanho do href
+                        sizeString = (sizeLine) - (startLine + 4); //define o tamanho da string
+                        lineLink = line.Substring(startLine, sizeString); //usa linelink para armazenar somente a parte que sera buscada o pdf
+                        sizeLine = lineLink.Length; //guarda o tamanho do linelink
+                        finishLine = lineLink.IndexOf(Extension); //marca o final do trecho onde esta o .pdf
+                        finishLine = finishLine + Extension.Length; //marca novamente mas adicionando o tamanho da string .pdf de 4 posições
+                        lineLink = lineLink.Substring(0, finishLine); //agora tira até a parte do pdf, separando o link
+                        startSubstring = line.IndexOf(Extension); //aqui marca onde esta o .pdf para depois tirar esta parte do line para remover a parte ja extraida
+                                                                  //rtConteudo.AppendText(countLine + " | " + lineLink); //adiciona no textbox o numero da linha e | mais a linha
+                                                                  //rtConteudo.AppendText("\n"); //pula linha no textbox
+
+                        for (int i = 0; i < lineLink.Length; i++) //começa laço para ler a variavel lineLink toda
+                        {
+                            tempContent = tempContent + lineLink[i]; //vai adicionando caracter a caracter a variavel tempContent
+                                                                     //MessageBox.Show(tempContent);
+                            if (tempContent.Contains(HTTP) && tempContent.Contains("\">"))
+                            {
+                                int posNaoPDF = lineLink.IndexOf("\">"); //procura a posição do final da url sem pdf
+                                lineLink = lineLink.Substring(posNaoPDF + 2); //remove da variavel a parte de url invalida
+                                tempContent = ""; //limpa a variavel temporaria para receber novo valor                                
+                                break; //interrompe a execução para ler novos valores
+
+                            }
+                            if (tempContent.Contains(HTTP) && !tempContent.Contains("\">") && tempContent.Contains(Extension)) //verificando se a linha contem o http e final do link ">
+                            {
+                                try
+                                {
+                                    rtConteudo.AppendText(countLine + " | " + tempContent); //adiciona no textbox o numero da linha e | mais a linha
+                                    rtConteudo.AppendText(pl); //pula linha no textbox
+                                    tempContent = "";   //limpa a variavel temporaria para receber novo valor
+                                    countOccurrence++;
+                                }
+                                catch (Exception ex)
+                                {
+                                    MessageBox.Show(ex.ToString());
+                                }
+
+                            }
+                        }
+                        
+                    }
+                    line = line.Substring(startSubstring + Extension.Length); //aqui retira do line a parte ja procurada para procurar mais links dentro do line
+                }
+            }
+
+        }
+        //só um teste de leitura de char
+        public void stringVetorTest()
+        {
+            String tmpvar = "link http para arquivo pdf";
+            for (int i = 0; i < tmpvar.Length; i++)
+            {
+                MessageBox.Show("pos: "+ i + " = " + Convert.ToString(tmpvar[i]));
+
+                rtConteudo.AppendText(Convert.ToString(tmpvar[i]));
+            }
+
+            
+        }
+        #endregion
+
+        #region Botões
+        private void btGo_Click(object sender, EventArgs e)
+        {
+            if (cbPDF.Checked)
+            {
+                SearchReference(pdf);
+            }else if (cbRAR.Checked)
+            {
+                SearchReference(rar);
+            }else if (cbPDF.Checked && cbRAR.Checked)
+            {
+                SearchReference(pdf);
+                SearchReference(rar);
+            }
+            else
+            {
+                MessageBox.Show("Escolha ao menos uma extensão para buscar");
+            }
+            
+            //stringVetorTest();
+        }
+        #endregion
+
+        private void btOpen_Click(object sender, EventArgs e)
+        {
+            //Criar Janela com Open File Dialog para jogar na variavel htmlFile e exibir no tbox do caminho
+        }
+    }
+}
